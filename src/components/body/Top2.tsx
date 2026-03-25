8e124e43c728d6e59108ae3ef670e43595b71ba6
FaranRaja <faranraja011@gmail.com>
2026-03-25 13:43:13 +0500

    axios working, photos fetched

--- src/components/body/Top2.tsx
+++ src/components/body/Top2.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Flame , TrendingUp , Shuffle , Sparkles , MountainSnow } from 'lucide-react'
 import { useState } from 'react'
+import Wallpapers from './Wallpapers'
+
 
 
 enum Category {
@@ -24,15 +26,17 @@ const Top2 = () => {
   }
 
   return (
-    <div className='top-2'>
-        <button className={`diff-i ${getActive(Category.POPULAR)}`} onClick = {() => {handleSelection(Category.POPULAR)}}><Flame /> POPULAR</button>
-        <button className={`diff-i ${getActive(Category.TRENDING)}`} onClick = {() => {handleSelection(Category.TRENDING)}}><TrendingUp /> TRENDING</button>
-        <button className={` diff-i ${getActive(Category.RANDOM)}`} onClick = {() => {handleSelection(Category.RANDOM)}}><Shuffle /> RANDOM</button>
-        <button className={`diff-i ${getActive(Category.AESTHETIC)}`} onClick = {() => {handleSelection(Category.AESTHETIC)}}><Sparkles /> AESTHETIC</button>
-        <button className={`diff-i ${getActive(Category.NATURE)}`} onClick = {() => {handleSelection(Category.NATURE)}}><MountainSnow /> NATURE</button>
-        
-        
-      
+    <div>
+        <div className='top-2'>
+            <button className={`diff-i ${getActive(Category.POPULAR)}`} onClick = {() => {handleSelection(Category.POPULAR)}}><Flame /> POPULAR</button>
+            <button className={`diff-i ${getActive(Category.TRENDING)}`} onClick = {() => {handleSelection(Category.TRENDING)}}><TrendingUp /> TRENDING</button>
+            <button className={` diff-i ${getActive(Category.RANDOM)}`} onClick = {() => {handleSelection(Category.RANDOM)}}><Shuffle /> RANDOM</button>
+            <button className={`diff-i ${getActive(Category.AESTHETIC)}`} onClick = {() => {handleSelection(Category.AESTHETIC)}}><Sparkles /> AESTHETIC</button>
+            <button className={`diff-i ${getActive(Category.NATURE)}`} onClick = {() => {handleSelection(Category.NATURE)}}><MountainSnow /> NATURE</button>
+         </div>
+
+
+         <Wallpapers category = {selected} />
     </div>
   )
 }
---
+++ src/components/body/Wallpapers.tsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+type WallpaperProps = {
+  category: string;
+};
+
+type Src = {
+  original: string;	
+  large2x: string;
+  large: string;
+  medium: string;
+  small: string;
+  portrait: string;
+  landscape: string;
+  tiny: string;
+};
+
+type Photo = {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  photographer_url: string;
+  photographer_id: number;
+  avg_color: string;
+  src: Src;
+  liked: boolean;
+  alt: string;
+};
+
+type PexelsResponse = {
+  page: number;
+  per_page: number;
+  photos: Photo[];
+  total_results: number;
+  next_page: string;
+};
+
+const Wallpapers = ({ category }: WallpaperProps) => {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  const fetchPhotos = async () => {
+    try {
+      const res = await axios.get<PexelsResponse>(
+        "https://api.pexels.com/v1/search",
+        {
+          headers: {
+            Authorization: "oKA82CSj95nFmCE59dCgPuQw3DPmSR9wChnAqNyKgdI0fbgmYEkd16Ha",
+          },
+          params: {
+            query: category,
+            per_page: 300000,
+            page: 1,
+          },
+        }
+      );
+
+      setPhotos(res.data.photos);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [category]);
+
+  return (
+    <div className='wallpapers'>
+      {photos.map((photo) => (
+        <div key={photo.id} className='wallpaper'>
+          <img src={photo.src.original} alt={photo.alt} className='wallpaper-img' loading='lazy'/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Wallpapers;
